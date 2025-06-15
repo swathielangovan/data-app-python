0bbf77173fe34d2d7de35a41a1652d46b1a66385
swathielangovan <swathielango23@gmail.com>
2025-06-15 13:45:08 +0530

    Day 2: Update images for sky conditions

--- main.py
+++ main.py
@@ -4,20 +4,32 @@ from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place:")
-days = st.slider("Forecast Days", minvalue = 1, max_value = 5,
+days = st.slider("Forecast Days", min_value = 1, max_value = 5,
                  help = "Select the number of forecasted days")
 option = st.selectbox("Select data to view",
                       ("Temperature","Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-def get_data(days):
-    dates = ["2022-25-10","2022-26-10","2022-27-10"]
-    temperatures = [10,11,15]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
+# def get_data(days):
+#     dates = ["2022-25-10","2022-26-10","2022-27-10"]
+#     temperatures = [10,11,15]
+#     temperatures = [days * i for i in temperatures]
+#     return dates, temperatures
 
-data = get_data(place, days, option)
-d,t = get_data(days)
+if place:
+    filtered_data = get_data(place, days)
 
-figure = px.line(x=d, y=t, labels={"x":"Date", "y":"Temperatures (in C)"})
-st.plotly_chart(figure)
+if option == "Temperature":
+    # Create a temperature plot
+    temperatures = [dict["main"]["temp"] for dict in filtered_data]
+    dates = [dict["dt_txt"] for dict in filtered_data]
+    figure = px.line(x=dates, y=temperatures, labels={"x":"Date", "y":"Temperatures (in C)"})
+    st.plotly_chart(figure)
+
+if option == "Sky":
+    # Create a sky plot
+    images = {"Clear":"images/clear.png","Clouds":"images/cloud.png","Rain":"images/rain.png","Snow":"images/snow.png"}
+    sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+    image_paths = [images[condition] for condition in sky_conditions]
+    #print(sky)
+    st.image(image_paths,width = 100)
